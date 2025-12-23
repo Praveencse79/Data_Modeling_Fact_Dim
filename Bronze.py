c02772ade4758645c1213a806dad72270acefc35
Praveen Kumar <139849701+Praveencse79@users.noreply.github.com>
2025-12-23 19:35:45 +0530

    Delete Bronze.py

--- Bronze.py
+++
@@ -1,6 +0,0 @@
-if spark.catalog.tableExists("datamodeling.bronze.bronze_table"):
-    last_load_date = spark.sql("SELECT max(order_date) FROM datamodeling.bronze.bronze_table").collect()[0][0]
-else:
-    last_load_date = '1000-01-01'
-
-
